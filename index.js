8b056d2069787fdd98fc2668bcdfe0ebc07231f2
astralevolution <120140754+astralevolution@users.noreply.github.com>
2023-11-05 17:56:03 +0000

    ye

--- index.js
+++ index.js
@@ -7,21 +7,9 @@ const app = express();
 
 app.use(cors());
 
-// Serve a different HTML file for the root page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'style.css'));
-});
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'script.js'));
-});
-
-// Proxy middleware for all other routes
 app.use(createProxyMiddleware('/', {
-  target: 'https://now.gg',
+  target: 'https://www.openalgebra.com',
   changeOrigin: true,
   secure: false, // Bypass SSL/TLS verification
 }));
